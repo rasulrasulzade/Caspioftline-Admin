bcbf9ac39a94a18816fd0df6bcced3fd5eff0bc6
rasulrasulzada <rasul.rasulzada1@gmail.com>
2020-03-30 21:07:40 +0400

    added some pages

--- src/js/main.js
+++ src/js/main.js
@@ -3,9 +3,11 @@ import "../styles/main.sass"
 'use strict'
 import lazyload from "./lazyload"
 import navbar from "./navbar"
+import {membersAccordion} from "./members"
 
 $(document).ready(function(){
 lazyload()
 
 navbar();
+membersAccordion();
 });
---
+++ src/js/members.js
@@ -0,0 +1,36 @@
+const membersAccordion = () => {
+
+    var acc = document.getElementsByClassName("members-item__header");
+    console.log(acc)
+    var i;
+
+    const  closeOthers = n => {
+        for(let el of  acc) {
+        const panel = el.nextElementSibling;
+        if(el !== n)  {
+            panel.style.maxHeight = null;
+            panel.parentElement.classList.remove("accordion-item--active");
+        }
+           
+        }
+    }
+        
+    for (i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function() { 
+        closeOthers(this)
+        console.log(this)
+        // this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+         if (panel.style.maxHeight) {
+          panel.style.maxHeight = null;
+          panel.parentElement.classList.remove("accordion-item--active");
+        } else {
+          panel.style.maxHeight = panel.scrollHeight + "px";    
+          panel.parentElement.classList.add("accordion-item--active");
+        } 
+      });
+    }
+
+}
+
+export {membersAccordion}
